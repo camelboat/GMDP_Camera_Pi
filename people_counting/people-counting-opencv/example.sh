a47519ba38760b15d0092986518c1c7ee07f7d5a
camelboat <camelboat@sjtu.edu.cn>
2019-03-01 08:48:38 +0000

    新文件：   people_counting/dlib_test.py 	新文件：   people_counting/people-counting-opencv/example.sh 	新文件：   people_counting/people-counting-opencv/mobilenet_ssd/MobileNetSSD_deploy.caffemodel 	新文件：   people_counting/people-counting-opencv/mobilenet_ssd/MobileNetSSD_deploy.prototxt 	新文件：   people_counting/people-counting-opencv/people_counter.py 	新文件：   people_counting/people-counting-opencv/pyimagesearch/centroidtracker.py 	新文件：   people_counting/people-counting-opencv/pyimagesearch/trackableobject.py 	新文件：   people_counting/shape_predictor_68_face_landmarks.dat

---
+++ people_counting/people-counting-opencv/example.sh
@@ -0,0 +1,3 @@
+!/bin/bash
+
+python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/test_2.h264 --output output/output_try03.avi
